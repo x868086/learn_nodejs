8cf70dc88a844d1f79c4e14eefdedc07a47eeb85
x868086 <43070810@qq.com>
2023-03-09 23:07:13 +0800

    lession14  encapsulation routers&static

--- http_demo_01/index.js
+++ http_demo_01/index.js
@@ -1,8 +1,6 @@
 const http = require('http')
 const URL = require('url').URL
 const path = require('path')
-const ejs = require('ejs')
-const {open} = require('fs/promises')
 const routers = require('./routers/index.js')
 
 
@@ -10,68 +8,25 @@ const routers = require('./routers/index.js')
 let httpServer = http.createServer()
 
 httpServer.on('request', async (req, res) => {
-    // 1.调用自定义staticFile方法，根据url请求中的path来确定是否读取静态文件并返回给请求端
-    routers.staticFile('static', req, res)
+
     let urlObj = new URL(req.url, `http://${req.headers.host}`);
     let pathName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
-    let fileExtname = path.extname(pathName) 
-    // 2.如果url的path中不带文件后缀名，则读取path对应ejs模板
-    if(!fileExtname){
-        if (pathName === '/login'){
-            let msg = "数据库里面获取的数据";
-            let item = [
-                {
-                    title: '新闻111'
-                },
-                {
-                    title: '新闻222'
-                },
-                {
-                    title: '新闻3333'
-                },
-                {
-                    title: '新闻4444'
-                }, {
-                    title: '新闻5555'
-                }
-            ]
-            ejs.renderFile('./views/login.ejs',{msg:msg,item:item},(err, str)=>{
-                res.writeHead(200,{'Content-Type':'text/html;charset="utf-8"'});
-                res.write(str)
-                res.end()
-            })
-        }else if (pathName == '/news') {
-                // /news?page=1&name=5
-                let querySearch = urlObj
-            ejs.renderFile('./views/form.ejs',{},(err, str)=>{
-                res.writeHead(200,{'Content-Type':'text/html;charset="utf-8"'});
-                res.write(str)
-                // 获取/news?page=1&name=5 的get传值
-                res.write(`Param1:  ${querySearch.searchParams.get('page')} `)
-                res.write(`Param2:  ${querySearch.searchParams.get('name')} `)
-                res.end()
-            })
-        } else if (pathName == '/doLogin') {
-            let postData =''
-            req.on('data',(chunk)=>{
-                postData += chunk
-            });
-            req.on('end',()=>{
-                res.writeHead(200,{'Content-Type':'text/html;charset="utf-8"'});
-                res.write(postData)
-                res.write(`请求方法是：${req.method}`)
-                res.end()
-            })
+    let fileExtname = path.extname(pathName)
 
-        } else {
-            let fileHandle = await open(`./static/404.html`, 'r')
-            let file = await fileHandle.readFile()
-            fileHandle.close()
-            res.writeHead(404, { "Content-Type": "text/html;charset='utf-8'" })
-            res.write(file)
-            res.end()
+    // 1.调用自定义staticFile方法，根据url请求中的path判断请求是否是带后缀名的静态文件
+    // 如果是指向静态文件则调用staticFile方法
+    if(fileExtname){
+        await routers.staticFile('static', req, res)
+    } else {
+    // 2.如果url的path中不带文件后缀名，则调用router模块封装的方法，根据pathName来匹配对应的路由
+        let pathstr = pathName.replace("/","")
+        try {
+            routers[pathstr](req,res)  
+        } catch (error) {
+            routers['error'](req,res)
         }
     }
+
 })
 
 
--- http_demo_01/routers/index.js
+++ http_demo_01/routers/index.js
@@ -1,32 +1,97 @@
 const { open } = require('fs/promises')
 const URL = require('url').URL
 const path = require('path')
+const ejs = require('ejs')
 const { getMimeType } = require('../utils/common')
 
-exports.staticFile = async function (filepath, req, res) {
-    let urlObj = new URL(req.url, `http://${req.headers.host}`);
-    // 1.1获取path,将没有具体指向的/指向到/index.html首页
-    let pathName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
 
-    // 1.2获取path中路径的扩展名，根据扩展名调用自定义的getMimeType方法，来确定向请求端返回何种格式的文件
-    let fileExtname = path.extname(pathName)
-    let mimeType = await getMimeType(fileExtname)
-    try {
-        let fileHandle = await open(`./${filepath}/${pathName}`, 'r')
-        let file = await fileHandle.readFile()
-        fileHandle.close()
 
-        res.writeHead(200, { "Content-Type": `${mimeType};charset='utf-8'` })
-        res.write(file)
-        res.end()
-        // 1.3 如果获取文件失败，则返回404静态页面
-    } catch (error) {
-        let fileHandle = await open(`./${filepath}/404.html`, 'r')
+// 将处理路由的业务逻辑封装到app对象并导出
+let app = {
+    staticFile: async function (filepath, req, res) {
+        let urlObj = new URL(req.url, `http://${req.headers.host}`);
+        // 1.1获取path,将没有具体指向的/指向到/index.html首页
+        let pathName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
+
+        // 1.2获取path中路径的扩展名，根据扩展名调用自定义的getMimeType方法，来确定向请求端返回何种格式的文件
+        let fileExtname = path.extname(pathName)
+        let mimeType = await getMimeType(fileExtname)
+        try {
+            let fileHandle = await open(`./${filepath}/${pathName}`, 'r')
+            let file = await fileHandle.readFile()
+            fileHandle.close()
+
+            res.writeHead(200, { "Content-Type": `${mimeType};charset='utf-8'` })
+            res.write(file)
+            res.end()
+            // 1.3 如果获取文件失败，则返回404静态页面
+        } catch (error) {
+            let fileHandle = await open(`./${filepath}/404.html`, 'r')
+            let file = await fileHandle.readFile()
+            fileHandle.close()
+            res.writeHead(404, { "Content-Type": "text/html;charset='utf-8'" })
+            res.write(file)
+            res.end()
+
+        }
+    },
+    login: (req, res) => {
+        let msg = "数据库里面获取的数据";
+        let item = [
+            {
+                title: '新闻111'
+            },
+            {
+                title: '新闻222'
+            },
+            {
+                title: '新闻3333'
+            },
+            {
+                title: '新闻4444'
+            }, {
+                title: '新闻5555'
+            }
+        ]
+        ejs.renderFile('./views/login.ejs', { msg: msg, item: item }, (err, str) => {
+            res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
+            res.write(str)
+            res.end()
+        })
+    },
+    doLogin: (req, res) => {
+        let postData = ''
+        req.on('data', (chunk) => {
+            postData += chunk
+        });
+        req.on('end', () => {
+            res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
+            res.write(postData)
+            res.write(`请求方法是：${req.method}`)
+            res.end()
+        })
+    },
+    news: function (req, res) {
+        // /news?page=1&name=5
+        let urlObj = new URL(req.url, `http://${req.headers.host}`);
+        let querySearch = urlObj
+        ejs.renderFile('./views/form.ejs', {}, (err, str) => {
+            res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
+            res.write(str)
+            // 获取/news?page=1&name=5 的get传值
+            res.write(`Param1:  ${querySearch.searchParams.get('page')} `)
+            res.write(`Param2:  ${querySearch.searchParams.get('name')} `)
+            res.end()
+        })
+    },
+    error: async (req, res) => {
+        let fileHandle = await open(`./static/404.html`, 'r')
         let file = await fileHandle.readFile()
         fileHandle.close()
         res.writeHead(404, { "Content-Type": "text/html;charset='utf-8'" })
         res.write(file)
         res.end()
-
     }
 }
+
+module.exports = app
